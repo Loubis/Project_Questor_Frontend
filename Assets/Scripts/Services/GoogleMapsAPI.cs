03fa7094970b41a248f52fa3940a7ca91722de90
Steffen O <steffenosterburg@googlemail.com>
2018-07-21 12:28:21 +0200

    added GPS to GoogleMaps | added Screen with to Maps API

--- Assets/Scripts/Services/GoogleMapsAPI.cs
+++ Assets/Scripts/Services/GoogleMapsAPI.cs
@@ -9,8 +9,8 @@ public class GoogleMapsAPI : MonoBehaviour {
 
     string url;
 
-    public float lat;
-    public float lng;
+    public float latitude;
+    public float longitude;
 
     LocationInfo li;
 
@@ -24,7 +24,7 @@ public class GoogleMapsAPI : MonoBehaviour {
 
     IEnumerator Map()
     {
-        url = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lng +
+        url = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude.ToString() + "," + longitude.ToString() +
             "&zoom=" + zoom + "&size=" + mapWidth + "x" + mapHeight + "&scale=" + scale
             + "&maptype=" + mapSelected +
             "&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyDqCjII5YABMxpueTxa0Ao4VH6E8MsYN2E";
@@ -33,15 +33,26 @@ public class GoogleMapsAPI : MonoBehaviour {
         img.texture = www.texture;
         img.SetNativeSize();
     }
-
+    
 	// Use this for initialization
 	void Start () {
         img = gameObject.GetComponent<RawImage>();
+        // Get GPS location
+        this.longitude = GPS.Instance.longitude;
+        this.latitude = GPS.Instance.latitude;
+        
+        // Set map scale to screen width
+        this.mapWidth = Screen.width;
+        this.mapHeight = Screen.height;
+
+
         StartCoroutine(Map());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        // Update GPS locations
+		this.longitude = GPS.Instance.longitude;
+        this.latitude = GPS.Instance.latitude;
 	}
 }
