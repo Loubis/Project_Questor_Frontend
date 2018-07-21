2d1e77fb1a873309fd5af0c29ac9d342e21e69d9
Drakon94 <ciezkonr@gmail.com>
2018-07-21 15:48:53 +0200

    Image Scaler
    Wrote ImageScaler Script in Services. Now scales map according to left right. Still many bugs but better than before

--- Assets/Scripts/Services/GoogleMapsAPI.cs
+++ Assets/Scripts/Services/GoogleMapsAPI.cs
@@ -26,12 +26,12 @@ public class GoogleMapsAPI : MonoBehaviour
 
         url = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lng +
             "&zoom=" + zoom + "&size=" + mapWidth + "x" + mapHeight + "&scale=" + scale
-            + "&maptype=" + mapSelected +
-            "&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyDqCjII5YABMxpueTxa0Ao4VH6E8MsYN2E";
+            + "&maptype=" + mapSelected + "&markers=color:blue%7Clabel:S%7C" + lat + "," + lng + "&key=AIzaSyDqCjII5YABMxpueTxa0Ao4VH6E8MsYN2E";
         WWW www = new WWW(url);
         yield return www;
         img.texture = www.texture;
-        img.SetNativeSize();
+        //img.SetNativeSize();
+        ImageScaler.SizeImgToParent(img);
     }
     // Use this for initialization
     void Start()
