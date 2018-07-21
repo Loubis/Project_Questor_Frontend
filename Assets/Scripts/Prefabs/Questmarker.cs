900f13d20b079f25f2092326774d1b0d678180e9
Steffen O <steffenosterburg@googlemail.com>
2018-07-21 22:19:44 +0200

    Fixed project structure

---
+++ Assets/Scripts/Prefabs/Questmarker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mapbox.Unity.Location;
+using Mapbox.Utils;
+using UnityEngine;
+
+public class Questmarker : MonoBehaviour {
+
+	bool _isInitialized;
+	public double longitude;
+	public double latitude;
+	
+	void Start()
+	{
+		LocationProviderFactory.Instance.mapManager.OnInitialized += () => _isInitialized = true;
+	}
+
+	void LateUpdate()
+	{
+		if (_isInitialized)
+		{
+			var map = LocationProviderFactory.Instance.mapManager;
+			transform.localPosition = map.GeoToWorldPosition(new Vector2d(longitude, latitude));
+		}
+	}
+}
