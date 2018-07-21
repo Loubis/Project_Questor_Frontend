8d60a96aa30ce750ce53667c68f06fb6552cb068
Steffen O <steffenosterburg@googlemail.com>
2018-07-22 00:30:41 +0200

    added QuestPopUp on Startmarker click

--- Assets/Scripts/Prefabs/Questmarker.cs
+++ Assets/Scripts/Prefabs/Questmarker.cs
@@ -3,24 +3,32 @@ using System.Collections.Generic;
 using Mapbox.Unity.Location;
 using Mapbox.Utils;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Questmarker : MonoBehaviour {
+public class Questmarker : MonoBehaviour
+{
 
-	bool _isInitialized;
-	public double longitude;
-	public double latitude;
-	
-	void Start()
-	{
-		LocationProviderFactory.Instance.mapManager.OnInitialized += () => _isInitialized = true;
-	}
+    bool _isInitialized;
+    public double longitude;
+    public double latitude;
 
-	void LateUpdate()
-	{
-		if (_isInitialized)
-		{
-			var map = LocationProviderFactory.Instance.mapManager;
-			transform.localPosition = map.GeoToWorldPosition(new Vector2d(longitude, latitude));
-		}
-	}
+	public GameObject questPopUp;
+
+    void Start()
+    {
+        LocationProviderFactory.Instance.mapManager.OnInitialized += () => _isInitialized = true;
+    }
+
+    void LateUpdate()
+    {
+        if (_isInitialized)
+        {
+            var map = LocationProviderFactory.Instance.mapManager;
+            transform.localPosition = map.GeoToWorldPosition(new Vector2d(longitude, latitude));
+        }
+    }
+    void OnMouseDown()
+    {
+		questPopUp.SetActive(true);
+    }
 }
